2a7e2a206b64c480ec1814e5394b76dcce96d96e
olegior <10z@tut.by>
2024-01-31 19:47:05 +0300

    tag changed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,13 +9,13 @@ export const Header = memo(({ limit, setLimit }) => {
     return (
         <header className="header">
             <div className="header__container container">
-                <menu className="header__menu">
+                <nav className="header__menu">
                     <NavLink className='header__link' to={'/'}><img className="header__logo" src={pokeball} alt="to homepage"
                         style={{ height: 40, width: 40 }}
                     /></NavLink>
                     <NavLink className='header__link header__link--yellow' to={'pagination'}>Pagination</NavLink>
                     <NavLink className='header__link header__link--yellow' to={'infinite'}>Infinite</NavLink>
-                </menu>
+                </nav>
                 <NumberInput {...{ defaultValue: limit, setValue: setLimit, max: 96, min: 16, step: 8, span: 'limit', button: 'set' }} />
             </div>
         </header>
